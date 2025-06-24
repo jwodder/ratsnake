9393d33c5f520154e9757dd6a06bab27c8c5b91e
John T. Wodder II <git@varonathe.org>
2025-06-24 13:34:18 -0400

    Document `highscores.rs`

--- src/highscores.rs
+++ src/highscores.rs
@@ -5,10 +5,18 @@ use std::collections::HashMap;
 use std::num::NonZeroU32;
 use thiserror::Error;
 
+/// A collection of the highest score achieved for various `Options` values
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub(crate) struct HighScores(HashMap<Options, NonZeroU32>);
 
 impl HighScores {
+    /// Save the high scores to a file on disk
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the data directory path could not be determined, if
+    /// creating the file's parent directories failed, if serializing the high
+    /// scores failed, or if writing the serialized high scores failed.
     pub(crate) fn save(&self) -> Result<(), SaveError> {
         let path = high_scores_file_path().ok_or_else(SaveError::no_path)?;
         if let Some(parent) = path.parent().filter(|p| !p.as_os_str().is_empty()) {
@@ -20,6 +28,14 @@ impl HighScores {
         Ok(())
     }
 
+    /// Read high scores from a file on disk.  If the file does not exist, an
+    /// empty `HighScores` value is returned.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the data directory path could not be determined, if
+    /// the file could not be read, or if the file's contents could not be
+    /// deserialized.
     pub(crate) fn load() -> Result<HighScores, LoadError> {
         let path = high_scores_file_path().ok_or_else(LoadError::no_path)?;
         let src = match fs_err::read(&path) {
@@ -30,14 +46,19 @@ impl HighScores {
         serde_json::from_slice(&src).map_err(LoadError::deserialize)
     }
 
+    /// Return the high score, if any, for the given [`Options`]
     pub(crate) fn get(&self, opts: Options) -> Option<NonZeroU32> {
         self.0.get(&opts).copied()
     }
 
+    /// Set the high score for `opts` to `score`.  No attempt is made to verify
+    /// that `score` is higher than the current high score.
     pub(crate) fn set(&mut self, opts: Options, score: NonZeroU32) {
         self.0.insert(opts, score);
     }
 
+    /// Convert the high scores to a list of `{"options": ..., "score": ...}`
+    /// objects that can then be serialized as JSON
     fn to_json_array(&self) -> Vec<HighScoreEntry> {
         self.0
             .iter()
@@ -45,6 +66,8 @@ impl HighScores {
             .collect()
     }
 
+    /// Convert a list of `{"options": ..., "score": ...}` objects to a
+    /// `HighScores` instance
     fn from_json_array(array: Vec<HighScoreEntry>) -> HighScores {
         HighScores(
             array
@@ -67,12 +90,15 @@ impl<'de> Deserialize<'de> for HighScores {
     }
 }
 
+/// An intermediate type used for serializing & deserializing `HighScores` as
+/// JSON
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 struct HighScoreEntry {
     options: Options,
     score: NonZeroU32,
 }
 
+/// Error returned by [`HighScores::save()`]
 #[derive(Debug, Error)]
 #[error("Failed to save high scores to disk")]
 pub(crate) struct SaveError(#[source] SaveErrorSource);
@@ -95,6 +121,19 @@ impl SaveError {
     }
 }
 
+/// Source error of [`SaveError`].
+///
+/// Implementing this as separate type allows for error displays like the
+/// following, with a general message at the top level and a source message
+/// describing which part of the operation failed:
+///
+/// ```text
+/// Failed to save high scores to disk
+///
+/// Caused by:
+///     0: failed to create parent directories
+///     1: permission denied
+/// ```
 #[derive(Debug, Error)]
 enum SaveErrorSource {
     #[error("failed to determine path to local data directory")]
@@ -107,8 +146,9 @@ enum SaveErrorSource {
     Write(#[source] std::io::Error),
 }
 
+/// Error returned by [`HighScores::load()`]
 #[derive(Debug, Error)]
-#[error("Failed to read high scores from disk")]
+#[error("Failed to load high scores from disk")]
 pub(crate) struct LoadError(#[source] LoadErrorSource);
 
 impl LoadError {
@@ -125,6 +165,19 @@ impl LoadError {
     }
 }
 
+/// Source error of [`LoadError`].
+///
+/// Implementing this as separate type allows for error displays like the
+/// following, with a general message at the top level and a source message
+/// describing which part of the operation failed:
+///
+/// ```text
+/// Failed to load high scores from disk
+///
+/// Caused by:
+///     0: failed to read high scores file
+///     1: permission denied
+/// ```
 #[derive(Debug, Error)]
 enum LoadErrorSource {
     #[error("failed to determine path to local data directory")]
