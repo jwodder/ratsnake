b2bb7c824ff85f16365aa4f704a34861c73f7401
John T. Wodder II <git@varonathe.org>
2025-06-24 13:23:44 -0400

    Document `command.rs`

--- src/command.rs
+++ src/command.rs
@@ -1,26 +1,45 @@
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
+/// An enum of input commands, (mostly) abstracted away from the key codes that
+/// produce them
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub(crate) enum Command {
+    /// Quit the program (Ctrl-C)
     Quit,
+    /// Move up (Up, `w`, `k`, `8`)
     Up,
+    /// Move down (Down, `s`, `j`, `2`)
     Down,
+    /// Move left (Left, `a`, `h`, `4`)
     Left,
+    /// Move right (Right, `d`, `l`, `6`)
     Right,
+    /// Select/activate the current button or menu item
     Enter,
+    /// User pressed the spacebar
     Space,
+    /// Go to the first item in the list (Home)
     Home,
+    /// Go to the last item in the list (End)
     End,
+    /// Go to the next item, circling around at the end (Tab)
     Next,
+    /// Go to the previous item, circling around at the beginning (Shift+Tab)
     Prev,
+    /// User pressed the Escape key
     Esc,
+    /// User pressed the `m` key
     M,
+    /// User pressed the `p` key
     P,
+    /// User pressed the `q` key
     Q,
+    /// User pressed the `r` key
     R,
 }
 
 impl Command {
+    /// Return the `Command`, if any, for the given key event
     pub(crate) fn from_key_event(ev: KeyEvent) -> Option<Command> {
         match (ev.modifiers, ev.code) {
             (KeyModifiers::CONTROL, KeyCode::Char('c')) => Some(Command::Quit),
